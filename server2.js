54199dd09ee13513a9d1e2f49b22a168b828f14f
Jefferson Abraham Dermawan <jeffersonabrahamdermawan1217@gmail.com>
2026-06-15 14:22:35 +0700

    get req body for post

--- server2.js
+++ server2.js
@@ -40,6 +40,25 @@ const getUserByIdHandler = (req, res) => {
     res.end();
 }
 
+// Route Handler for POST /api/users
+const createUserHandler = (req, res) => {
+    let body = "";
+    // Listen for data
+    req.on('data', (chunk) => {
+        body += chunk.toString();
+    });
+
+    req.on('end', () => {
+        const newUser =  JSON.parse(body);
+        users.push(newUser);
+        res.statusCode = 201;
+        res.write(JSON.stringify(newUser));
+        res.end();
+    })
+}
+
+// Not found Handler
+
 const notFoundHandler = (req, res) => {
     res.statusCode = 404;
     res.write(JSON.stringify({message: "User Not Found"}));
@@ -54,6 +73,9 @@ const server = createServer((req,res) => {
                 getUsersHandler(req, res);
             } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
                 getUserByIdHandler(req , res);
+            } else if (req.url === '/api/users' && req.method === 'POST') {
+                createUserHandler(req, res);
+            
             } else {
                 notFoundHandler(req, res);
             }
