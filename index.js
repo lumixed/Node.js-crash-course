5da2fc6b1379a54f77080cb64b4a5b3cbfcb9435
Jefferson Abraham Dermawan <jeffersonabrahamdermawan1217@gmail.com>
2026-06-15 13:58:27 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import getPosts, {getPostsLength} from './postController.js';
+
+console.log(getPosts());
+console.log(`Posts Length : ${getPostsLength()}`);
+
+// const {generateRandomNumber , celciusToFarhenheit } = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius To Farhenheit : ${celciusToFarhenheit(0)}`);
---
+++ server2.js
@@ -0,0 +1,73 @@
+import {createServer} from 'http';
+const PORT = process.env.PORT;
+
+const users = [
+    {id: 1, name: "John Doe"},
+    {id: 2, name: "Jane Doe"},
+    {id: 3, name: "Jim Doe"}
+]
+
+
+// Logger middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+// JSON middleware
+const jsonMiddleware = (req, res, next) => {
+            res.setHeader("Content-type", 'application/json');
+            next();
+}
+
+// Route handler for GET /api/users
+const getUsersHandler = (req, res) => {
+    res.write(JSON.stringify(users));
+    res.end();
+}
+
+// Route handler for GET /api/users/:id 
+const getUserByIdHandler = (req, res) => {
+    const id = req.url.split("/")[3];
+    const user = users.find((user) => user.id == parseInt(id));
+
+    if (user) {
+        res.write(JSON.stringify(user));
+    } else {
+        res.statusCode = 404;
+        res.write(JSON.stringify({message: "User Not Found"}));
+    }
+    res.end();
+}
+
+const notFoundHandler = (req, res) => {
+    res.statusCode = 404;
+    res.write(JSON.stringify({message: "User Not Found"}));
+}
+const server = createServer((req,res) => {
+    logger(req, res, ()=> {
+        if (req.url == "/api/users" && req.method == 'GET') {
+            res.write(JSON.stringify(users));
+            res.end();
+        } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+            const id = req.url.split("/")[3];
+            const user = users.find((user) => user.id == parseInt(id));
+            res.setHeader("Content-type", 'application/json');
+            if (user) {
+                res.write(JSON.stringify(user));
+            } else {
+
+            }
+            res.end();
+        } else {
+            res.setHeader("Content-type", 'application/json');
+            res.statusCode = 404;
+            res.write(JSON.stringify({message: "Route Not Found"}));
+            res.end();
+        }
+    });
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
